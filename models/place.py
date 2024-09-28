30b80cdd5b97e8348af22a5ac9a7789a510828c7
Mketia <k.mukakalis1@alustudent.com>
2024-09-28 16:08:03 +0200

    updated amenity class

--- models/place.py
+++ models/place.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
-from models.base_model import BaseModel
+"""User class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, DateTime, Column, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class Place(BaseModel):
-    """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+class User(BaseModel, Base):
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+    """
+    __tablename__ = 'users'
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship(
+        'Place',
+        backref='user',
+        cascade='all, delete-orphan')
+    reviews = relationship(
+        'Review',
+        backref='user',
+        cascade='all, delete-orphan')
