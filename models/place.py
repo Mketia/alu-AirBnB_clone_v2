5f89170bdfe3892ca77a79a601198430830198b3
Mketia <k.mukakalis1@alustudent.com>
2024-09-28 16:37:33 +0200

    updated amenity class

--- models/place.py
+++ models/place.py
@@ -1,29 +1,71 @@
 #!/usr/bin/python3
-"""User class"""
+""" Place Module for HBNB project """
+import os
+
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, DateTime, Column, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
+from models.review import Review
+from models.amenity import Amenity
+import models
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60),
+                             ForeignKey('places.id'),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'),
+                             primary_key=True, nullable=False))
 
 
-class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
-    """
-    __tablename__ = 'users'
+class Place(BaseModel, Base):
+    """ A place to stay """
+    __tablename__ = 'places'
 
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    places = relationship(
-        'Place',
-        backref='user',
-        cascade='all, delete-orphan')
-    reviews = relationship(
-        'Review',
-        backref='user',
-        cascade='all, delete-orphan')
+    city_id = Column(String(60), ForeignKey("cities.id", ondelete="CASCADE"),
+                     nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id", ondelete="CASCADE"),
+                     nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+    amenity_ids = []
+
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship("Review", backref="place")
+        amenities = relationship("Amenity", secondary="place_amenity",
+                                 viewonly=False,
+                                 back_populates="place_amenities")
+
+    if os.getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def reviews(self):
+            """Returns the list of Review instances with place_id equals
+            to the current Place.id."""
+
+            reviews = list(models.storage.all(Review).values())
+
+            return list(
+                filter(lambda review: (review.place_id == self.id), reviews))
+
+        @property
+        def amenities(self):
+            """Returns the list of Amenity instances based on
+            the attribute amenity_ids that contains all Amenity.id."""
+
+            amenities = list(models.storage.all(Amenity).values())
+
+            return list(
+                filter(lambda amenity: (amenity.place_id in self.amenity_ids),
+                       amenities))
+
+        @amenities.setter
+        def amenities(self, value=None):
+            """Adds ids in amenity_ids ."""
+            if isinstance(value, type(Amenity)):
+                self.amenity_ids.append(value.id)
