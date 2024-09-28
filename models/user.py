1078e64b37deac349f75b71f5f0f508bcdef2436
YvetteGahamanyi <ygahamanyi26@gmail.com>
2024-09-28 18:19:10 +0200

    ft: fix tests

--- models/user.py
+++ models/user.py
@@ -1,45 +1,30 @@
 #!/usr/bin/python3
-"""Defines the State class."""
-import models
-from os import getenv
+"""Defines the User class."""
 from models.base_model import Base
 from models.base_model import BaseModel
-from models.city import City
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
 
 
-class State(BaseModel, Base):
-    """Represents a state for a MySQL database.
+class User(BaseModel, Base):
+    """Represents a user for a MySQL database.
 
-    Inherits from SQLAlchemy Base and links to the MySQL table states.
+    Inherits from SQLAlchemy Base and links to the MySQL table users.
 
     Attributes:
-        __tablename__ (str): The name of the MySQL table to store States.
-        name (sqlalchemy String): The name of the State.
-        cities (sqlalchemy relationship): The State-City relationship.
+        __tablename__ (str): The name of the MySQL table to store users.
+        email: (sqlalchemy String): The user's email address.
+        password (sqlalchemy String): The user's password.
+        first_name (sqlalchemy String): The user's first name.
+        last_name (sqlalchemy String): The user's last name.
+        places (sqlalchemy relationship): The User-Place relationship.
+        reviews (sqlalchemy relationship): The User-Review relationship.
     """
-    __tablename__ = "states"
+    __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    cities = relationship("City",  backref="state", cascade="delete")
-    # places = relationship(
-    #     'Place',
-    #     backref='user',
-    #     cascade='all, delete-orphan')
-    # reviews = relationship(
-    #     'Review',
-    #     backref='user',
-    #     cascade='all, delete-orphan')
-    if getenv("HBNB_TYPE_STORAGE") != "db":
-        @property
-        def cities(self):
-            """Get a list of all related City objects."""
-            city_list = []
-            for city in list(models.storage.all(City).values()):
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -31,23 +31,9 @@ class test_fileStorage(unittest.TestCase):
     def test_new(self):
         """ New object is correctly added to __objects """
         new = BaseModel()
-        new.save()  # Explicitly save the object to add it to storage
-        temp = None
         for obj in storage.all().values():
             temp = obj
-        self.assertIsNotNone(temp, "The object was not added to storage")
         self.assertTrue(temp is obj)
-
-    def test_key_format(self):
-        """ Key is properly formatted """
-        new = BaseModel()
-        new.save()  # Ensure the object is saved before checking the key format
-        _id = new.to_dict()['id']
-        temp = None
-        for key in storage.all().keys():
-            temp = key
-        self.assertIsNotNone(temp, "No object found in storage")
-        self.assertEqual(temp, 'BaseModel' + '.' + _id)
 
     def test_all(self):
         """ __objects is properly returned """
@@ -77,14 +63,10 @@ class test_fileStorage(unittest.TestCase):
     def test_reload(self):
         """ Storage file is successfully loaded to __objects """
         new = BaseModel()
-        new.save()  # Ensure the object is saved
-        storage.save()  # Persist data to 'file.json'
-        storage.reload()  # Reload objects from 'file.json'
-
-        loaded = None
+        storage.save()
+        storage.reload()
         for obj in storage.all().values():
             loaded = obj
-        self.assertIsNotNone(loaded, "The object was not reloaded into storage")
         self.assertEqual(new.to_dict()['id'], loaded.to_dict()['id'])
 
     def test_reload_empty(self):
@@ -111,6 +93,14 @@ class test_fileStorage(unittest.TestCase):
     def test_type_objects(self):
         """ Confirm __objects is a dict """
         self.assertEqual(type(storage.all()), dict)
+
+    def test_key_format(self):
+        """ Key is properly formatted """
+        new = BaseModel()
+        _id = new.to_dict()['id']
+        for key in storage.all().keys():
+            temp = key
+        self.assertEqual(temp, 'BaseModel' + '.' + _id)
 
     def test_storage_var_created(self):
         """ FileStorage object storage created """
