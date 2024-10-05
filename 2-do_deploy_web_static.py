a07f51c0c75e418f703d25d0c8c0cca4bdd244b1
YvetteGahamanyi <ygahamanyi26@gmail.com>
2024-10-05 14:02:08 +0200

    fix: PEP8

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -47,5 +47,3 @@ def do_deploy(archive_path):
            format(name)).failed is True:
         return False
     return True
-    print("Deploying to the server")
-    run("echo Deployed to /data/web_static/releases/{}/".format(name))
