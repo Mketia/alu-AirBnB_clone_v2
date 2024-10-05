82eb03b57825024df1ddca1feccf08c0bf9d7d9e
Mketia <k.mukakalis1@alustudent.com>
2024-10-05 17:59:05 +0200

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
-"""Fabric script which generates a tgz archive"""
+"""
+Fabric script that generates a tgz archive of the 'web_static' folder.
+"""
 
 from datetime import datetime
 from fabric.api import local
 from os.path import isdir
+
+
 def do_pack():
-    """TGZ"""
+    """
+    Generates a tgz archive from the contents of the 'web_static' folder.
+    Archive will be stored in the 'versions' folder.
+    """
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
+        
+        # Ensure 'versions' directory exists
+        if not isdir("versions"):
             local("mkdir versions")
+        
         file_name = "versions/web_static_{}.tgz".format(date)
+        
+        # Create the archive
         local("tar -cvzf {} web_static".format(file_name))
+        
         return file_name
-    except:
+    except Exception as e:
+        print(f"Error occurred: {e}")
         return None
